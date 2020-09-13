1b946b68bd1c3bab49efea2d4af40ad2d64b9247
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-09-13 02:31:18 -0400

    feat: added clearer logging

--- autobooker.py
+++ autobooker.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+import logging
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
@@ -31,10 +31,16 @@ try:
     password_input.send_keys(os.getenv("F4L_PASSWORD"))
     driver.implicitly_wait(5)
     password_input.send_keys(Keys.ENTER)
-    WebDriverWait(driver, 10)
+    driver.implicitly_wait(5)
+    logging.info("Logged In!")
 
-    # Switch to 2 days ahead
-    driver.find_element_by_id("btn_date_select").click()  # day selector
+    date_select = driver.find_element_by_id("btn_date_select")
+
+    if not date_select.is_displayed():
+        logging.info("Already booked max slots")
+        driver.quit()
+
+    date_select.click()  # day selector
     driver.implicitly_wait(3)
     driver.find_element_by_id("date_" + booking_date).click()  # select 2 days ahead from now
     driver.implicitly_wait(5)
@@ -47,17 +53,17 @@ try:
 
     for slot in available_slots:
         if str(os.getenv("TIME_SLOT")) in slot.text:
-            print("Time slot found: ", slot.text)
+            logging.info("Time slot found: ", slot.text)
             slot.find_element_by_xpath('..').click()
             driver.implicitly_wait(3)
             driver.find_element_by_id("dialog_book_yes").click()
             driver.implicitly_wait(5)
-            print("Reservation done!")
+            logging.info("Reservation done!")
             break
         else:
-            print("Skipping slot:", slot.text)
+            logging.info("Skipping slot:", slot.text)
 
 except Exception as err:
-    print(str(err))
+    logging.warning(str(err))
 finally:
     driver.quit()
